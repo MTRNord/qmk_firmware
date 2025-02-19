74d43bdda9235aa9aa11b795efd9f2b88731a937
MTRNord <mtrnord1@gmail.com>
2025-02-19 17:46:35 +0100

    Fix the main things of the qmk keymap

--- keyboards/zsa/moonlander/keymaps/mtrnord/config.h
+++ keyboards/zsa/moonlander/keymaps/mtrnord/config.h
@@ -2,7 +2,8 @@
   Set any config.h overrides for your specific keymap here.
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
-#define ORYX_CONFIGURATOR
+#pragma once
+// #define ORYX_CONFIGURATOR
 #undef DEBOUNCE
 #define DEBOUNCE 10
 
